42d3a1375c5d1345ba864a1d3907308a5a84b53a
Vladimir Berdnikov <gazetter@mail.ru>
2021-03-04 23:57:20 +0700

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task1
+{
+   // Наша задача — сравнить производительность вставки в List<T> и LinkedList<T>.Для этого используйте уже знакомый вам StopWatch.
+
+   //На примере этого текста, выясните, какие будут различия между этими коллекциями.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
