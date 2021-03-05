95def6a97cf1c021eeff092825d6ff99efbd7ccb
Vladimir Berdnikov <gazetter@mail.ru>
2021-03-05 18:59:11 +0700

    Готово замеряет среднее из 10 попыток

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
 namespace Task1
 {
@@ -10,7 +13,46 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // читаем весь файл в строку 
+            string text = File.ReadAllText(@"c:\test\Text.txt");
+            // разбиваем в массив, используя пробел в качестве разделителя
+            var words = text.Split(" ");
+            List<string> simpleList = new List<string>();
+            LinkedList<string> linkedList = new LinkedList<string>();
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            double mean = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                timer.Restart();
+                foreach (var item in words)
+                {
+                    simpleList.Add(item);
+                }
+                timer.Stop();
+                mean += timer.Elapsed.TotalMilliseconds;
+                Console.WriteLine("Время затраченное на добавление элементов в List<T>:" + timer.Elapsed.TotalMilliseconds);
+            }
+            Console.WriteLine("\nСреднее время затраченное на добавление элементов в List<T>: " + mean / 10 + "\n");
+
+            mean = 0; 
+            for (int i = 0; i < 10; i++)
+            {
+                timer.Restart();
+                foreach (var item in words)
+                {
+                    linkedList.AddLast(item);
+                }
+                timer.Stop();
+                mean += timer.Elapsed.TotalMilliseconds;
+                Console.WriteLine("Время затраченное на добавление элементов в LinkedList<T>:" + timer.Elapsed.TotalMilliseconds);
+
+            }
+            Console.WriteLine("\nСреднее время затраченное на добавление элементов в LinkedList<T>: " + mean / 10);
+
+            Console.ReadKey();
+
         }
     }
 }
