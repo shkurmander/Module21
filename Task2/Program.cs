2ef3cb36a422a20b079d9b1fdc727b8dac392010
Vladimir Berdnikov <gazetter@mail.ru>
2021-03-05 22:50:05 +0700

    Готово, Работает согласно ТЗ даже чуть больше

---
+++ Task2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Task2
+{
+    /// Ваша задача — написать программу, которая позволит понять, какие 10 слов чаще всего встречаются в тексте.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // читаем весь файл в строку 
+            string text = File.ReadAllText(@"c:\test\Text.txt");
+            // разбиваем в массив, используя пробел в качестве разделителя
+            var words = text.Split(" ");
+
+            //инициализируем словарь ключом будет слово, значением количество упоминаний
+            Dictionary<string, int> wordDictionary = new Dictionary<string, int>();
+
+            foreach (var item in words)
+            {
+                if (wordDictionary.ContainsKey(item))
+                    wordDictionary[item]++;
+                else
+                {
+                    char symbol;
+                    if (item.Length > 1)                   
+                        wordDictionary.Add(item, 1);                    
+                    else
+                        if (Char.TryParse(item, out symbol) && !Char.IsPunctuation(symbol))                        
+                            wordDictionary.Add(item, 1);                    
+                       
+                }
+            }
+            var orderedDictionary = from record in wordDictionary
+                        where record.Value > 1
+                        orderby record.Value descending
+                        select record;
+            int max = 15;
+            int cnt = 1;
+
+            Console.WriteLine($"{max} слов наиболее часто встречающиеся в тексте: ");
+            foreach (var item in orderedDictionary)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} раз");
+                ++cnt;
+                if (cnt > max)
+                    break;
+            }
+            Console.WriteLine($"Всего уникальных слов в тексте:  { orderedDictionary.Count()}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ефыл2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ефыл2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
